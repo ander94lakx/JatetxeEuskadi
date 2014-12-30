f474b8963a8663d4ef083e7f1590a92a27e0dba5
ander94lakx <andergranado@outlook.com>
2014-12-30 12:53:02 +0100

    Creado el servlet para guardar los datos modificados del perfil

---
+++ src/java/packServlets/ServletPerfil.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packServlets;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Asus
+ */
+public class ServletPerfil extends HttpServlet {
+
+    Connection conn = null;
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+        super.init(config);
+        
+        if(config.getServletContext().getAttribute("CONEXION") == null) {
+            String dsn = "jdbc:odbc:"+config.getServletContext().getInitParameter("BD");
+            try {
+              Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+              conn = DriverManager.getConnection(dsn, "", "");
+            } catch(ClassNotFoundException ex) {
+              System.out.println("Error al cargar el driver");
+            } catch (SQLException sqlEx) {
+              System.out.println("Se ha producido un error al establecer la conexión con: " + dsn);
+            }
+        } else {
+            conn = (Connection) config.getServletContext().getAttribute("CONEXION");
+        }
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
