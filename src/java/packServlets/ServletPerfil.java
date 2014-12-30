54c06380dc5663335f4fcb47e337bff0a3863313
ander94lakx <andergranado@outlook.com>
2014-12-30 13:08:25 +0100

    Implementado servlet que guarda las modificaciones en el perfil

--- src/java/packServlets/ServletPerfil.java
+++ src/java/packServlets/ServletPerfil.java
@@ -18,6 +18,20 @@ import javax.servlet.http.HttpServletResponse;
  * @author Asus
  */
 public class ServletPerfil extends HttpServlet {
+    String email = "";
+    String dni = "";
+    char sexo = ' ';
+    String nombre = "";
+    String apellido = "";
+    String provincia = "";
+    String ciudad = "";
+    int codigopostal = 0;
+    int telefono = 0;
+    short dia = 0;
+    short mes = 0;
+    short ano = 0;
+    String direccion = "";
+    String imagen = "";
 
     Connection conn = null;
     
@@ -43,8 +57,120 @@ public class ServletPerfil extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        boolean existeError = false;
+        if(!request.getParameter("email").equals(""))
+            email = request.getParameter("email");
+        else
+            existeError = true;
         
+        if(!request.getParameter("dni").equals(""))
+            dni = request.getParameter("dni");
+        else
+            existeError = true;
+         
+         if(!request.getParameter("sexo").equals(""))
+            sexo =  request.getParameter("sexo").charAt(0);
+        else
+            existeError = true;
         
+        if(!request.getParameter("nombre").equals(""))
+            nombre = request.getParameter("nombre");
+        else
+            existeError = true;
+           
+        if(!request.getParameter("apellido").equals(""))
+            apellido = request.getParameter("apellido");
+        else
+            existeError = true;
+        
+        if(!request.getParameter("provincia").equals(""))
+            provincia = request.getParameter("provincia");
+        else
+            existeError = true;
+         
+        if(!request.getParameter("ciudad").equals(""))
+            ciudad = request.getParameter("ciudad");
+        else
+            existeError = true;
+           
+        if(!request.getParameter("codigopostal").equals(""))
+            codigopostal = Integer.parseInt(request.getParameter("codigopostal"));
+        else
+            existeError = true;
+        
+        if(!request.getParameter("telefono").equals(""))
+            telefono = Integer.parseInt(request.getParameter("telefono"));
+        else
+            existeError = true;
+        
+        if(!request.getParameter("dia").equals(""))
+            dia = Short.parseShort(request.getParameter("dia"));
+        else
+            existeError = true;
+        
+        if(!request.getParameter("mes").equals(""))
+            mes = Short.parseShort(request.getParameter("mes"));
+        else
+            existeError = true;
+        
+        if(!request.getParameter("ano").equals(""))
+            ano = Short.parseShort(request.getParameter("ano"));
+        else
+            existeError = true;
+        
+        if(!request.getParameter("direccion").equals(""))
+            direccion = request.getParameter("direccion");
+        else
+            existeError = true;
+        
+        if(!request.getParameter("cimagen").equals(""))
+            imagen = request.getParameter("imagen");
+        else
+            existeError = true;
+        
+        if(!existeError) {
+            if(actualizarUsuarioBD()){
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+            } else {
+                String error = "Error al modificar los datos";
+                request.getSession(true).setAttribute("errorPerfil", error);
+                request.getRequestDispatcher("/perfil.jsp").forward(request, response);
+            }
+            
+        }
+        else
+            System.out.println("Se ha producido un error");
+    }
+    
+    private boolean actualizarUsuarioBD() {
+        Statement st = null;
+        try{
+            st= conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT email FROM Usuario");
+            if(rs.getString("email") == null) {
+                return false;
+            } else {
+                st.executeQuery("UPDATE Usuario SET dni='"+dni+"',"+
+                                                   "sexo='"+sexo+"',"+
+                                                   "nombre='"+nombre+"',"+
+                                                   "apellido='"+apellido+"',"+
+                                                   "provincia='"+provincia+"',"+
+                                                   "ciudad='"+ciudad+"',"+
+                                                   "codigopostal="+codigopostal+","+
+                                                   "telefono'"+telefono+","+
+                                                   "dia="+dia+","+
+                                                   "mes="+mes+","+
+                                                   "ano="+ano+","+
+                                                   "direccion='"+direccion+"',"+
+                                                   "imagen='"+imagen+"'"+
+                                "WHERE email='"+email+"';");
+                return true;
+            }
+        } catch(SQLException sql){
+            System.out.println("Se produjo un errror creando el Statement");
+            System.out.println(sql.getMessage());
+        }
+        return false;
     }
 
     @Override
